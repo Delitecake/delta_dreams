62a26d45eba5435be393fa61a02c275e45cd6615
Jalyna <jalyna.schroeder@gmail.com>
2016-08-13 21:26:57 +0200

    add authentication

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
+  validates :username, presence: true,
+    uniqueness: { case_sensitive: true },
+    format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :terms_of_service, acceptance: true
+end
